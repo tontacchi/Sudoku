b47b98f596cf334236b0a66d69e697844b758fbc
tontacchi <andy.tonthat14@gmail.com>
2025-05-17 16:47:16 -0700

    added Constraint interface and AllDiffConstraint struct

--- solver/constraint.go
+++ solver/constraint.go
@@ -1,1 +1,70 @@
 package solver
+
+// generic CSP constraint interface
+type Constraint interface {
+	Variables()   []*Variable
+	IsSatisfied() bool
+	isModified()  bool
+	String()      string
+}
+
+// Constraint: all values in the set are unique
+type AllDiffConstraint struct {
+	variables []*Variable
+}
+
+func NewAllDiffConstraint(variables []*Variable) *AllDiffConstraint {
+	return &AllDiffConstraint{
+		variables: variables,
+	}
+}
+
+// Mutators
+func (c *AllDiffConstraint) AddVariable(variable *Variable) {
+	c.variables = append(c.variables, variable)
+}
+
+// Accessors & Constraint Interface
+func (c *AllDiffConstraint) Variables() []*Variable {
+	return c.variables
+}
+
+func (c *AllDiffConstraint) IsModified() bool {
+	for _, variable := range c.variables {
+		if variable.modified { return true }
+	}
+
+	return false
+}
+
+func (c *AllDiffConstraint) IsSatisfied() bool {
+	set := make(map[int]bool)
+
+	for _, variable := range c.variables {
+		if !variable.assigned { continue }
+
+		assignment := variable.Assignment()
+		if set[assignment] {
+			return false
+		}
+
+		set[assignment] = true
+	}
+
+	return true
+}
+
+func (c *AllDiffConstraint) String() string {
+	repr := "{"
+
+	for index, variable := range c.variables {
+		if index > 0 {
+			repr += ", "
+		}
+
+		repr += variable.String()
+	}
+
+	return repr
+}
+
