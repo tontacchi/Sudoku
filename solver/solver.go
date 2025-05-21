448c279a50ac9eca7724315d00075e4786e7c5b7
tontacchi <andy.tonthat14@gmail.com>
2025-05-20 18:01:56 -0700

    started BacktrackSolver w/ dependency injected strategy interfaces

--- solver/solver.go
+++ solver/solver.go
@@ -1,2 +1,52 @@
 package solver
 
+import (
+	"time"
+)
+
+// Strategy Interfaces
+type VarSelector interface {
+	Select(*Network) *Variable
+}
+
+type ValSelector interface {
+	OrderValues(*Variable, *Network) []int
+}
+
+type ConsistencyChecker interface {
+	Enforce(*Network, *Trail) bool
+}
+
+
+// Backtrack Solver w/ injected strategy
+
+type BacktrackSolver struct {
+	Network     *Network
+	Trail       *Trail
+	HasSolution bool
+
+	VarSelector         // Select()
+	ValSelector         // OrderValues()
+	ConsistencyChecker  // Enforce()
+}
+
+func NewBacktrackSolver(
+	network     *Network,
+	trail       *Trail,
+	varSelector VarSelector,
+	valSelector ValSelector,
+	checker     ConsistencyChecker,
+) *BacktrackSolver {
+	return &BacktrackSolver{	
+		Network:            network,
+		Trail:              trail,
+		VarSelector:        varSelector,
+		ValSelector:        valSelector,
+		ConsistencyChecker: checker,
+		HasSolution:        false,
+	}
+}
+
+
+// Solver Logic
+
