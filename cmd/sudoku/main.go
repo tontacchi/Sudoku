b1067bdd772bc8c52775e977a65e8ef042b1ccc9
tontacchi <andy.tonthat14@gmail.com>
2025-05-23 12:36:59 -0700

    minimum working board builder and solver woot

--- cmd/sudoku/main.go
+++ cmd/sudoku/main.go
@@ -8,29 +8,33 @@ import (
 )
 
 func main() {
-	const NUM_ROWS  = 3
-	const NUM_COLS  = 2
-	const NUM_HINTS = 10
-	const SOLVE_TIME_LIMIT = time.Hour * 2
+	const NUM_ROWS  = 4
+	const NUM_COLS  = 4
+	const NUM_HINTS = 120
+	const SOLVE_TIME_LIMIT = time.Minute * 2
 
-	board   := sudoku.NewRandomBoard(NUM_ROWS, NUM_COLS, NUM_HINTS)
+	fmt.Println("Creating sudoku board...")
+	board   := sudoku.NewBoardFromSolved(NUM_ROWS, NUM_COLS, NUM_HINTS)
 	network := sudoku.NewNetworkFromBoard(board)
 	trail   := solver.NewTrail()
-
-	varSelector := solver.MRVWithDegree{}
+	
+	varSelector := solver.MRV{}
 	valSelector := solver.LeastConstrainingValue{}
-	checker     := solver.NorvigCheck{}
+	checker     := solver.ForwardChecking{}
 
-	solver  := solver.NewBacktrackSolver(network, trail, varSelector, valSelector, checker)
+	solver := solver.NewBacktrackSolver(network, trail, varSelector, valSelector, checker)
 
+	fmt.Println("Done!")
 	fmt.Printf("starting board:\n%v\n", board.String())
 
 	start  := time.Now()
 	result := solver.Solve(SOLVE_TIME_LIMIT)
 	after  := time.Now()
 
-	board = sudoku.NewBoardFromNetwork(solver.Network, NUM_ROWS, NUM_COLS)
-	fmt.Printf("final board:\n%v\n", board.String())
+	board = sudoku.NewBoardFromNetwork(network, NUM_ROWS, NUM_COLS)
+	if solver.HasSolution {
+		fmt.Printf("final board:\n%v\n", board.String())
+	}
 
 	fmt.Printf("solution found: %v\n", result)
 	fmt.Printf("solving time elapsed: %v\n", after.Sub(start))
--- sudoku/builder.go
+++ sudoku/builder.go
@@ -2,6 +2,8 @@ package sudoku
 
 import (
 	"sudoku-csp/solver"
+	"math/rand/v2"
+	"time"
 	"fmt"
 )
 
@@ -16,8 +18,26 @@ func NewNetworkFromBoard(board *Board) *solver.Network {
 		for col := range boardLen {
 			value    := board.Cells[row][col]
 			domain   := domainFor(value, boardLen)
-			boxIndex := (row / board.BoxRows) * board.BoxCols + (col / board.BoxCols)
-			fmt.Printf("(%d, %d) assigned box %d\n", row, col, boxIndex)
+
+			// boxIndex := (row % board.BoxRows) * board.BoxRows + (col / board.BoxCols)
+			// fmt.Printf("(%d / %d) * %d + (%d / %d) -> %d\n", row, board.BoxRows, board.BoxCols, col, board.BoxCols, boxIndex)
+
+			boxColsCount := board.BoardLen() / board.BoxCols
+			boxIndex := (row / board.BoxRows) * boxColsCount + (col / board.BoxCols)
+
+			// boxIndex := (row / board.BoxRows) * board.BoxCols + (col / board.BoxCols)
+			// fmt.Printf("(%d / %d) * %d + (%d / %d) -> %d\n", row, board.BoxRows, board.BoxRows, col, board.BoxCols, boxIndex)
+
+			// rowOffset := row / board.BoxRows
+			// colOffset := col / board.BoxCols
+			//
+			// [0][0] = 0
+			// [0][1] = 1
+			// [1][0] = 2  // (2, 1) 
+			// [1][1] = 3
+
+			// boxIndex := (col / board.BoxCols) * board.BoxRows + (row / board.BoxRows)
+			// fmt.Printf("(%d, %d) assigned box %d\n\n", row, col, boxIndex)
 			variable := solver.NewVariable(domain, row, col, boxIndex)
 
 			tempVars = append(tempVars, variable)
@@ -36,12 +56,12 @@ func NewNetworkFromBoard(board *Board) *solver.Network {
 		boxGroups[variable.Block] = append(boxGroups[variable.Block], variable)
 	}
 
-	fmt.Printf("vars in row: %d, vars in col: %d, vars in box: %d\n", len(rowGroups), len(colGroups), len(boxGroups))
-	for i := range boardLen {
-		fmt.Printf("Row %d has %d vars\n", i, len(rowGroups[i]))
-		fmt.Printf("Col %d has %d vars\n", i, len(colGroups[i]))
-		fmt.Printf("Box %d has %d vars\n", i, len(boxGroups[i]))
-	}
+	// fmt.Printf("vars in row: %d, vars in col: %d, vars in box: %d\n", len(rowGroups), len(colGroups), len(boxGroups))
+	// for i := range boardLen {
+	// 	fmt.Printf("Row %d has %d vars\n", i, len(rowGroups[i]))
+	// 	fmt.Printf("Col %d has %d vars\n", i, len(colGroups[i]))
+	// 	fmt.Printf("Box %d has %d vars\n", i, len(boxGroups[i]))
+	// }
 
 	// 3) assign constraints for rows, cols, & boxes
 	for _, group := range []map[int][]*solver.Variable{rowGroups, colGroups, boxGroups} {
@@ -77,6 +97,84 @@ func NewBoardFromNetwork(network *solver.Network, boxRows, boxCols int) *Board {
 	}
 }
 
+func NewForwardCheckGeneratedBoard(boxRows, boxCols, numHints int) *Board {
+	board   := NewEmptyBoard(boxRows, boxCols)
+	network := NewNetworkFromBoard(board)
+	trail   := solver.NewTrail()
+
+	varSelector := solver.MRV{}
+	valSelector := solver.LeastConstrainingValue{}
+	checker     := solver.ForwardChecking{}
+
+	numAssigned := 0
+	
+	for numAssigned < numHints {
+		variable := varSelector.Select(network)
+		if variable == nil { break }
+
+		values := valSelector.OrderValues(variable, network)
+		rand.Shuffle(len(values), func(left, right int) {
+			values[left], values[right] = values[right], values[left]
+		})
+
+		placedValue := false
+		for _, value := range values {
+			trail.PlaceMarker()
+			trail.Push(variable)
+
+			variable.AssignValue(value)
+
+			if checker.Enforce(network, trail) {
+				numAssigned++
+				placedValue = true
+
+				break
+			}
+
+			trail.Undo()
+		}
+
+		if !placedValue { break }
+	}
+
+	return NewBoardFromNetwork(network, boxRows, boxCols)
+}
+
+func NewBoardFromSolved(boxRows, boxCols, numHints int) *Board {
+	totalCells := boxRows * boxCols * boxRows * boxCols
+	
+	// fix later
+	if numHints > totalCells {
+		numHints = totalCells
+	}
+
+	board   := NewEmptyBoard(boxRows, boxCols)
+	network := NewNetworkFromBoard(board)
+	trail   := solver.NewTrail()
+	
+	varSelector := solver.MRV{}
+	valSelector := solver.LeastConstrainingValue{}
+	checker     := solver.ForwardChecking{}
+
+	solver := solver.NewBacktrackSolver(network, trail, varSelector, valSelector, checker)
+	solver.Solve(time.Minute * 2)
+
+	board = NewBoardFromNetwork(network, boxRows, boxCols)
+	fmt.Printf("[ DEBUG ] the solved board:\n%v\n", board)
+	
+	positions := allCellCoords(board.BoardLen())
+	rand.Shuffle(len(positions), func(left, right int) {
+		positions[left], positions[right] = positions[right], positions[left]
+	})
+
+	for _, pair := range positions[:len(positions) - numHints] {
+		row, col := pair[0], pair[1]
+		board.Cells[row][col] = 0
+	}
+
+	return board
+}
+
 func domainFor(value, size int) []int {
 	if value != 0 {
 		return []int{value}
@@ -88,5 +186,19 @@ func domainFor(value, size int) []int {
 	}
 
 	return domain
+}
+
+func allCellCoords(dimension int) [][]int {
+	numPairs := dimension * dimension
+
+	pairs := make([][]int, numPairs)
+
+	for row := range dimension {
+		for col := range dimension {
+			pairs[row * dimension + col] = []int{row, col}
+		}
+	}
+
+	return pairs
 }
 
